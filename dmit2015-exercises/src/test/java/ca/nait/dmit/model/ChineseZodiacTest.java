6c4092d018eea09a83382434e6d8e3ce7e68d6ef
bfarhangmehr1 <bfarhangmehr1@nait.ca>
2018-09-13 14:43:43 -0600

    adding ChineseZodiacTest

---
+++ dmit2015-exercises/src/test/java/ca/nait/dmit/model/ChineseZodiacTest.java
@@ -0,0 +1,15 @@
+package ca.nait.dmit.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChineseZodiacTest {
+
+	@Test
+	public void testAnimal() {
+		fail("Not yet implemented");
+	}
+	
+
+}
