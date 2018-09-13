b75dd476f8b356fb4495594f44578a3049a0a4e6
bfarhangmehr1 <bfarhangmehr1@nait.ca>
2018-09-13 13:38:29 -0600

    Adding two more methods

--- dmit2015-exercises/src/main/java/ca/nait/dmit/model/ChineseZodiac.java
+++ dmit2015-exercises/src/main/java/ca/nait/dmit/model/ChineseZodiac.java
@@ -11,14 +11,35 @@ public class ChineseZodiac {
 		this.birthYear = birthYear;
 	}
 
-	public ChineseZodiac() {
-       birthYear = 1900;
-	}
-
-	public ChineseZodiac(int birthYear) {
-		super();
-		this.birthYear = birthYear;
-	}
-	 
-
+	 public String animal( int birthYear){
+		  String currentAnimal = "Unknown";			
+		  int Animal = (birthYear-1900) % 12;
+		   
+		  if (Animal == 0 ){
+			  currentAnimal = "Rat";
+		  } else if(Animal == 1) {
+			  currentAnimal = "Ox";
+		  }else if(Animal == 2) {
+			  currentAnimal = "Tiger";
+		  }else if(Animal == 3) {
+			  currentAnimal = "Rabbit";
+		  }else if(Animal == 4) {
+			  currentAnimal = "Dragon";
+		  }else if(Animal == 5) {
+			  currentAnimal = "Snake";
+		  }else if(Animal == 6) {
+			  currentAnimal = "Horse";
+		  }else if(Animal == 7) {
+			  currentAnimal = "Goat";
+		  }else if(Animal == 8) {
+			  currentAnimal = "Monkey";
+		  }else if(Animal == 9) {
+			  currentAnimal = "Rooster";
+		  }else if(Animal == 10) {
+			  currentAnimal = "Dog";
+		  }else {			  
+			  currentAnimal = "Pig";
+		  }		  
+		  return currentAnimal;
+	 }
 }
