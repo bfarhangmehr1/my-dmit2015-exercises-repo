225bb73cc4d3e5491d2e8be3818be6b8a8adc9d3
bfarhangmehr1 <bfarhangmehr1@nait.ca>
2018-09-17 16:09:46 -0600

    Adding banck account and pom

--- dmit2015-exercises/src/test/java/ex01/BankAccountTest.java
+++ dmit2015-exercises/src/test/java/ex01/BankAccountTest.java
@@ -14,13 +14,13 @@ public class BankAccountTest {
 		
 		BankAccount  account1 = new BankAccount();
 		
-		// set the balcne to 1000 
+		// set the balance to 1000 
 		account1.setBalance(BigDecimal.valueOf(1000));
-		// the balance sjhould be 1000;
+		// the balance should be 1000;
 		assertEquals(1000.00, account1.getBalance().doubleValue(),0);
 		// set the balance to -5000
 		account1.setBalance(BigDecimal.valueOf(-5000));
-		// the balance shoudl be still 1000
+		// the balance should be still 1000
 		assertEquals(1000.00, account1.getBalance().doubleValue(),0);
 		
 		
@@ -28,12 +28,12 @@ public class BankAccountTest {
 	@Test
 	public void testMonthlyInterstRate(){
 		BankAccount  account1 = new BankAccount();
-		// set the balnace to 1000
+		// set the balance to 1000
 		account1.setBalance(BigDecimal.valueOf((1000)));
 		// set annul rate to 1.5
 		account1.setAnnualInterestRate(1.5);
-		// the monthly interstr rate should be 
-		assertEquals(0.125, account1.getMonthlyInterestRate(),0);
+		// the monthly interest rate should be 
+		assertEquals(1.25, account1.getMonthlyInterestRate(),0);
 	}
 
 }
