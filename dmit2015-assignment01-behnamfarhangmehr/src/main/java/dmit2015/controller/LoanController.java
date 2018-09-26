8d92715c1e99478d4102b33c962363916e272d50
bfarhangmehr1 <bfarhangmehr1@nait.ca>
2018-09-26 16:55:45 -0600

    adding two

--- dmit2015-assignment01-behnamfarhangmehr/src/main/java/dmit2015/controller/LoanController.java
+++ dmit2015-assignment01-behnamfarhangmehr/src/main/java/dmit2015/controller/LoanController.java
@@ -1,8 +1,8 @@
 package dmit2015.controller;
 import java.io.Serializable;
 
-import javax.annotation.ManagedBean;
-import javax.annotation.PostConstruct;
+
+
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import org.primefaces.model.chart.Axis;
@@ -19,66 +19,54 @@ import dmit2015.model.LoanSchedule;
 
 @Named
 @ViewScoped
-@ManagedBean
 public class LoanController implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Loan currentLoan = new Loan();  // + getter 	
-	private BarChartModel loanChart;
+	private BarChartModel loanChart = new BarChartModel(); 
 	
-	@PostConstruct
-    public void init() {
-        createBarModels();
-    }
-	private BarChartModel initBarModel() {
-        BarChartModel model = new BarChartModel();
- 
-         double Amount = currentLoan.getMortgageAmout();
-         for (int amoritization = 1 ; amoritization <=currentLoan.getAmortizationPeriod(); amoritization++) {
-        	 LoanSchedule  link = new LoanSchedule ();
-        	 ChartSeries amor = new ChartSeries();        	 
-        	 amor.set(amoritization, Amount );
-        	 Amount = Amount-link.getPrinciplePaid(); 
-        	 model.addSeries(amor);
 
-         }
-         
-       return model;
-	}
-
-	    public BarChartModel getBarModel() {
-	        return loanChart;
-	    }
-	
-	    private void createBarModels() {
-	    	 createBarModel();
-	       
-	    }
-	    private void createBarModel() {
-	    	loanChart = initBarModel();
-	    	Axis xAxis = loanChart.getAxis(AxisType.X);
-	        xAxis.setLabel("Amoritization Period");
-	        xAxis.setMin(1);
-	        xAxis.setMax(currentLoan.getAmortizationPeriod());
-	        Axis yAxis = loanChart.getAxis(AxisType.Y);
-	        yAxis.setLabel("Mortgage Amount");
-	        yAxis.setMin(0);
-	        yAxis.setMax(currentLoan.getMortgageAmout());
-	    }
 	    
 	public void calculate() {		
 		Messages.addGlobalInfo(
 		 "Your montly payment is {0}", 
 		 currentLoan.getMounthlyPayment());	
 		
+		 double Amount = currentLoan.getMortgageAmout();
+         ChartSeries amor = null;
+         for (int amoritization = 1 ; amoritization <=currentLoan.getAmortizationPeriod(); amoritization++) {
+        	 LoanSchedule  link = new LoanSchedule ();
+        	  amor = new ChartSeries();        	 
+        	 amor.set(amoritization, Amount );	        	 
+        	 Amount = Amount-link.getPrinciplePaid();      	 
+        	       	 
+         }
+         loanChart.addSeries(amor);	 
+         
+    	Axis xAxis = loanChart.getAxis(AxisType.X);
+        xAxis.setLabel("Amoritization Period");
+        xAxis.setMin(1);
+        xAxis.setMax(currentLoan.getAmortizationPeriod());
+        Axis yAxis = loanChart.getAxis(AxisType.Y);
+        yAxis.setLabel("Mortgage Amount");
+        yAxis.setMin(0);
+        yAxis.setMax(currentLoan.getMortgageAmout());	
+	
 	}	
+
 	public Loan getCurrentLoan() {
 		return currentLoan;
-	}
+	}   
+	
 	public void setCurrentLoan(Loan currentLoan) {
 		this.currentLoan = currentLoan;
 	}
-	
+	public BarChartModel getLoanChart() {
+		return loanChart;
+	}
+	public void setLoanChart(BarChartModel loanChart) {
+		this.loanChart = loanChart;
+	}
 	
 
 
--- dmit2015-assignment01-behnamfarhangmehr/src/main/java/dmit2015/model/Loan.java
+++ dmit2015-assignment01-behnamfarhangmehr/src/main/java/dmit2015/model/Loan.java
@@ -3,78 +3,94 @@ package dmit2015.model;
 import java.util.ArrayList;
 
 public class Loan {
-  private double mortgageAmout;
-  private double annualInterestRate;
-  private int amortizationPeriod;
-  
-public double getMortgageAmout() {	 
-		return mortgageAmout;	
-	
-}
-public void setMortgageAmout(double mortgageAmout) {
-	if(mortgageAmout> 0) {
-		this.mortgageAmout = mortgageAmout;
-	}
-	
-}
-public double getAnnualInterestRate() {
-	return annualInterestRate;
-}
-public void setAnnualInterestRate(double annualInterestRate) {
-	if(annualInterestRate >0) {
-		this.annualInterestRate = annualInterestRate;
-	}
-	
-}
-public int getAmortizationPeriod() {
-	return amortizationPeriod;
-}
-public void setAmortizationPeriod(int amortizationPeriod) {
-	if(amortizationPeriod>0) {
-		this.amortizationPeriod = amortizationPeriod;
-	}	
-}
-public Loan() {
-	super();
-	this.amortizationPeriod = 1;
-	this.mortgageAmout = 1;
-	this.annualInterestRate = 0;
-}
-public Loan(double mortgageAmout, double annualInterestRate, int amortizationPeriod) {
-	super();
-	this.mortgageAmout = mortgageAmout;
-	this.annualInterestRate = annualInterestRate;
-	this.amortizationPeriod = amortizationPeriod;
-}
-public double getMounthlyPayment() {
-		
-	return mortgageAmout *(Math.pow(1+(annualInterestRate/200),(1.0/6.0))-1)/1- Math.pow(Math.pow(1+(annualInterestRate/200), 1.0/6.0), -12 * amortizationPeriod);
-}
+	private double mortgageAmout;
+	private double annualInterestRate;
+	private int amortizationPeriod;
 
- public  ArrayList<LoanSchedule> getLoanScheduleArray() {	 
-	 ArrayList<LoanSchedule> loansckedule = new ArrayList<LoanSchedule>();
-	    
-	    double remaingBalance = mortgageAmout;
-	   for (int paymentNumber = 1; paymentNumber < (12 * amortizationPeriod); paymentNumber++) {
-		LoanSchedule  link = new LoanSchedule ();
-		link.setPaymentNumber(paymentNumber);
-		link.setInterestPaid(Math.round(getMonthlyPercentageRate() * remaingBalance  *100.0)/100.0);
-		link.setPrinciplePaid(Math.round(getMounthlyPayment() * link. getInterestPaid() * 100.0)/100.0);
-		remaingBalance = remaingBalance - link.getPrinciplePaid();
-		link.setRemainingBalance(remaingBalance);
-		loansckedule.add(link);		
+	public double getMortgageAmout() {
+		return mortgageAmout;
+
+	}
+
+	public void setMortgageAmout(double mortgageAmout) {
+		if (mortgageAmout > 0) {
+			this.mortgageAmout = mortgageAmout;
 		}
-	
-	return loansckedule;
-    
-}
-	public double getMonthlyPercentageRate() {
-		return Math.round((Math.pow(1+(annualInterestRate/200), (1/6))-1) *100.0)/100.0;
+
 	}
 
-@Override
-public String toString() {
-	return "Loan [mortgageAmout=" + mortgageAmout + ", annualInterestRate=" + annualInterestRate
-			+ ", amortizationPeriod=" + amortizationPeriod + "]";
-}
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+
+	public void setAnnualInterestRate(double annualInterestRate) {
+		if (annualInterestRate > 0) {
+			this.annualInterestRate = annualInterestRate;
+		}
+
+	}
+
+	public int getAmortizationPeriod() {
+		return amortizationPeriod;
+	}
+
+	public void setAmortizationPeriod(int amortizationPeriod) {
+		if (amortizationPeriod > 0) {
+			this.amortizationPeriod = amortizationPeriod;
+		}
+	}
+
+	public Loan() {
+		super();
+		this.amortizationPeriod = 0;
+		this.mortgageAmout = 1;
+		this.annualInterestRate = 0;
+	}
+
+	public Loan(double mortgageAmout, double annualInterestRate, int amortizationPeriod) {
+		super();
+		this.mortgageAmout = mortgageAmout;
+		this.annualInterestRate = annualInterestRate;
+		this.amortizationPeriod = amortizationPeriod;
+	}
+
+	public double getMounthlyPayment() {
+
+		return Math.round(mortgageAmout * (Math.pow(1 + (annualInterestRate / 200), (1.0 / 6.0)) - 1)
+				/ (1 - Math.pow(Math.pow(1 + (annualInterestRate / 200), 1.0 / 6.0), -12 * amortizationPeriod)) * 100.0)
+				/ 100.0;
+	}
+
+	public ArrayList<LoanSchedule> getLoanScheduleArray() {
+		ArrayList<LoanSchedule> loansckedule = new ArrayList<LoanSchedule>();
+
+		double remaingBalance = mortgageAmout;
+		for (int paymentNumber = 1; paymentNumber < (12 * amortizationPeriod); paymentNumber++) {
+			LoanSchedule link = new LoanSchedule();
+			
+			link.setPaymentNumber(paymentNumber);
+			link.setInterestPaid(Math.round(getMonthlyPercentageRate() * remaingBalance * 100.0) / 100.0);
+			link.setPrinciplePaid(Math.round(getMounthlyPayment() - link.getInterestPaid() * 100.0) / 100.0);
+			if (remaingBalance > getMounthlyPayment()) {
+				remaingBalance = link.getPrinciplePaid();
+			}
+			remaingBalance = remaingBalance - link.getPrinciplePaid();
+			link.setRemainingBalance(remaingBalance);
+
+			loansckedule.add(link);
+		}
+
+		return loansckedule;
+
+	}
+
+	public double getMonthlyPercentageRate() {
+		return  Math.round( (Math.pow(1 + (annualInterestRate / 200), (1.0 / 6.0)) - 1) *100.0 / 100.0);
+	}
+
+	@Override
+	public String toString() {
+		return "Loan [mortgageAmout=" + mortgageAmout + ", annualInterestRate=" + annualInterestRate
+				+ ", amortizationPeriod=" + amortizationPeriod + "]";
+	}
 }
